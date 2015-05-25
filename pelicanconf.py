4e1a0cf7cb17a70b943ad7684591e7005430d307
Yax <kianby@madyanne.fr>
2015-05-26 00:01:07 +0200

    Remove cacause plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,13 +13,13 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'fr'
 TYPOGRIFY = False
 
-ARTICLE_EXCLUDES = (('pages','documents','extra','images'))
+ARTICLE_EXCLUDES = (('pages', 'documents', 'extra', 'images'))
 
 # feeds
 FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ATOM_CUSTOM = 'feeds/%s.atom.xml'
-#FEED_MAX_ITEMS = 10
+# FEED_MAX_ITEMS = 10
 
 # tags
 TAG_CLOUD_STEPS = 3
@@ -30,12 +30,12 @@ LINKS = ()
 SOCIAL = ()
 
 # piwik
-PIWIK_URL="www.madyanne.fr/piwik"
-PIWIK_SITE_ID=1
+PIWIK_URL = 'www.madyanne.fr/piwik'
+PIWIK_SITE_ID = 1
 
 # plugins
-PLUGIN_PATHS = ["plugins",]
-PLUGINS = ['sitemap','planet','cacause']
+PLUGIN_PATHS = ['plugins', ]
+PLUGINS = ['sitemap', 'planet']
 
 # Configure CaCause plugin
 CACAUSE_DIR = "comments"
@@ -55,59 +55,64 @@ SITEMAP = {
     }
 }
 
-STATIC_PATHS= [
-            'images',
-            'documents',
-            'extra/robots.txt',
-            'extra/redirect.html',
-            'extra/apple-touch-icon-120x120.png',
-            'extra/apple-touch-icon-114x114.png',
-            'extra/apple-touch-icon-144x144.png',
-            'extra/apple-touch-icon-152x152.png',
-            'extra/apple-touch-icon-57x57.png',
-            'extra/apple-touch-icon-60x60.png',
-            'extra/apple-touch-icon-72x72.png',
-            'extra/apple-touch-icon-76x76.png',
-            'extra/apple-touch-icon.png',
-            'extra/apple-touch-icon-precomposed.png',
-            'extra/browserconfig.xml',
-            'extra/favicon-160x160.png',
-            'extra/favicon-16x16.png',
-            'extra/favicon-196x196.png',
-            'extra/favicon-32x32.png',
-            'extra/favicon-96x96.png',
-            'extra/favicon.ico',
-            'extra/mstile-144x144.png',
-            'extra/mstile-150x150.png',
-            'extra/mstile-310x150.png',
-            'extra/mstile-310x310.png',
-            'extra/mstile-70x70.png'
+STATIC_PATHS = [
+    'images',
+    'documents',
+    'extra/robots.txt',
+    'extra/redirect.html',
+    'extra/apple-touch-icon-120x120.png',
+    'extra/apple-touch-icon-114x114.png',
+    'extra/apple-touch-icon-144x144.png',
+    'extra/apple-touch-icon-152x152.png',
+    'extra/apple-touch-icon-57x57.png',
+    'extra/apple-touch-icon-60x60.png',
+    'extra/apple-touch-icon-72x72.png',
+    'extra/apple-touch-icon-76x76.png',
+    'extra/apple-touch-icon.png',
+    'extra/apple-touch-icon-precomposed.png',
+    'extra/browserconfig.xml',
+    'extra/favicon-160x160.png',
+    'extra/favicon-16x16.png',
+    'extra/favicon-196x196.png',
+    'extra/favicon-32x32.png',
+    'extra/favicon-96x96.png',
+    'extra/favicon.ico',
+    'extra/mstile-144x144.png',
+    'extra/mstile-150x150.png',
+    'extra/mstile-310x150.png',
+    'extra/mstile-310x310.png',
+    'extra/mstile-70x70.png'
 ]
 EXTRA_PATH_METADATA = {
-            'extra/robots.txt': {'path': 'robots.txt'},
-            'extra/redirect.html': {'path': 'redirect.html'},
-            'extra/apple-touch-icon-114x114.png': {'path': 'apple-touch-icon-114x114.png'},
-            'extra/apple-touch-icon-120x120.png': {'path': 'apple-touch-icon-120x120.png'},
-            'extra/apple-touch-icon-144x144.png': {'path': 'apple-touch-icon-144x144.png'},
-            'extra/apple-touch-icon-152x152.png': {'path': 'apple-touch-icon-152x152.png'},
-            'extra/apple-touch-icon-57x57.png': {'path': 'apple-touch-icon-57x57.png'},
-            'extra/apple-touch-icon-60x60.png': {'path': 'apple-touch-icon-60x60.png'},
-            'extra/apple-touch-icon-72x72.png': {'path': 'apple-touch-icon-72x72.png'},
-            'extra/apple-touch-icon-76x76.png': {'path': 'apple-touch-icon-76x76.png'},
-            'extra/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
-            'extra/apple-touch-icon-precomposed.png': {'path': 'apple-touch-icon-precomposed.png'},
-            'extra/browserconfig.xml': {'path': 'browserconfig.xml'},
-            'extra/favicon-160x160.png': {'path': 'favicon-160x160.png'},
-            'extra/favicon-16x16.png': {'path': 'favicon-16x16.png'},
-            'extra/favicon-196x196.png': {'path': 'favicon-196x196.png'},
-            'extra/favicon-32x32.png': {'path': 'favicon-32x32.png'},
-            'extra/favicon-96x96.png': {'path': 'favicon-96x96.png'},
-            'extra/favicon.ico': {'path': 'favicon.ico'},
-            'extra/mstile-144x144.png': {'path': 'mstile-144x144.png'},
-            'extra/mstile-150x150.png': {'path': 'mstile-150x150.png'},
-            'extra/mstile-310x150.png': {'path': 'mstile-310x150.png'},
-            'extra/mstile-310x310.png': {'path': 'mstile-310x310.png'},
-            'extra/mstile-70x70.png': {'path': 'mstile-70x70.png'}
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/redirect.html': {'path': 'redirect.html'},
+    'extra/apple-touch-icon-114x114.png': {
+        'path': 'apple-touch-icon-114x114.png'},
+    'extra/apple-touch-icon-120x120.png': {
+        'path': 'apple-touch-icon-120x120.png'},
+    'extra/apple-touch-icon-144x144.png': {
+        'path': 'apple-touch-icon-144x144.png'},
+    'extra/apple-touch-icon-152x152.png': {
+        'path': 'apple-touch-icon-152x152.png'},
+    'extra/apple-touch-icon-57x57.png': {'path': 'apple-touch-icon-57x57.png'},
+    'extra/apple-touch-icon-60x60.png': {'path': 'apple-touch-icon-60x60.png'},
+    'extra/apple-touch-icon-72x72.png': {'path': 'apple-touch-icon-72x72.png'},
+    'extra/apple-touch-icon-76x76.png': {'path': 'apple-touch-icon-76x76.png'},
+    'extra/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'extra/apple-touch-icon-precomposed.png': {
+        'path': 'apple-touch-icon-precomposed.png'},
+    'extra/browserconfig.xml': {'path': 'browserconfig.xml'},
+    'extra/favicon-160x160.png': {'path': 'favicon-160x160.png'},
+    'extra/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+    'extra/favicon-196x196.png': {'path': 'favicon-196x196.png'},
+    'extra/favicon-32x32.png': {'path': 'favicon-32x32.png'},
+    'extra/favicon-96x96.png': {'path': 'favicon-96x96.png'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/mstile-144x144.png': {'path': 'mstile-144x144.png'},
+    'extra/mstile-150x150.png': {'path': 'mstile-150x150.png'},
+    'extra/mstile-310x150.png': {'path': 'mstile-310x150.png'},
+    'extra/mstile-310x310.png': {'path': 'mstile-310x310.png'},
+    'extra/mstile-70x70.png': {'path': 'mstile-70x70.png'}
 }
 
 DELETE_OUTPUT_DIRECTORY = False
